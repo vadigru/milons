d84ba74d46341bfb4087a47469dad0f0cc79571e
vadigru <vg@outlook.ie>
2018-08-12 14:42:42 +0300

    index and contacts

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let img = document.querySelector(`.logo>img`);
+let menu = document.querySelector(`.menu`);
+let wrapper = document.querySelector(`.wrapper`);
+
+wrapper.addEventListener(`click`, function (evt) {
+  if (evt.target === img) {
+    menu.classList.toggle(`hidden`);
+  }
+});
+
+document.addEventListener(`click`, function (evt) {
+  if (!evt.target.classList.contains(`menu_btn`) || evt.target.tagName === `img`) {
+    menu.classList.remove(`hidden`);
+  }
+});
